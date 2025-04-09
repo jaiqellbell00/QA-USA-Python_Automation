21e5a82b41897bc318ffbc3558faef186a02e36c
jaiqellbell00 <jaiqellbell00@gmail.com>
2025-04-09 09:02:15 -0500

    Project 7 1st submission

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-8e342edc-6903-41d4-bf8f-1624e75c9a83.containerhub.tripleten-services.com/'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '100 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
--- main.py
+++ main.py
@@ -1,2 +1,59 @@
+import data
+import helpers
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+        pass  # Placeholder for future code
+
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan")
+        pass  # Placeholder for future code
+
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number")
+        pass  # Placeholder for future code
+
+
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card")
+        pass  # Placeholder for future code
+
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+        pass  # Placeholder for future code
+
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for order blanket and handkerchief")
+        pass  # Placeholder for future code
+
+
+    def test_order_2_ice_creams(self):
+        for i in range(2):  # Loop will iterate twice
+            # Add in S8
+            print("function created for order 2 ice creams")
+            pass  # Placeholder for future code
+
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model appears")
+        pass  # Placeholder for future code
