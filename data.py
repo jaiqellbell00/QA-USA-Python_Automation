2ce840463cd990c539681005c3d1ed35166954d7
jaiqellbell00 <jaiqellbell00@gmail.com>
2025-05-15 00:04:10 -0500

    Project 8 1st submission

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # File with constants
-URBAN_ROUTES_URL = "https://cnt-33d53860-90e4-4f74-91cb-f7048eb99041.containerhub.tripleten-services.com/"
+URBAN_ROUTES_URL = ("https://cnt-33d53860-90e4-4f74-91cb-f7048eb99041.containerhub.tripleten-services.com/")
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
